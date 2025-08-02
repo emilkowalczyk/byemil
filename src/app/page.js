7d4f74d617ef30a7d3cdbe4eb49f6afcce8cac63
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2025-08-02 22:35:08 +0200

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,10 @@
 import React from 'react';
 
 export default function Home() {
-  const handleOnClickButton = () => alert('Elo');
-
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <h1>Hello world</h1>
       <p className='my-2'>Emil Kowalczyk</p>
       <p>Work in progress</p>
-      <button
-        onClick={handleOnClickButton}
-        className='mt-2 border-1 px-4 py-2 rounded-[4px] hover:bg-black hover:text-white cursor-pointer transition'
-      >
-        Click me
-      </button>
     </div>
   );
 }
